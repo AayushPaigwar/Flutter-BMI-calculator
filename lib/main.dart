d58a15101f79fc03f23257483ead75be187d3bec
Aayush Paigwar <aayushpaigwar@Aayushs-Air.bbrouter>
2023-06-13 00:52:31 +0530

    Files Added

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import 'input_page.dart';
+
+void main() {
+  runApp(const HomePage());
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: InputPage(),
+      theme: ThemeData.dark().copyWith(
+        appBarTheme: AppBarTheme(
+          backgroundColor: Color(0xff0a0e21),
+        ),
+        scaffoldBackgroundColor: Color(0xff0a0e21),
+        colorScheme: ColorScheme.dark().copyWith(secondary: Colors.purple),
+      ),
+    );
+  }
+}
